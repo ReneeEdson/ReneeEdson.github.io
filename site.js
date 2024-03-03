6859e2683a751c60dcd5d2dcd7194ddc6e32de4e
ReneeEdson <200138388@fvtc.edu>
2024-03-02 19:52:45 -0600

    Added secret message

---
+++ site.js
@@ -0,0 +1,33 @@
+console.log(document.querySelector('nav'))
+
+const footer = document.querySelector("footer")
+const copyright = footer.querySelector("p")
+    copyright.textContent = `${new Date().getFullYear()} All rights reserved.`;
+
+const links = document.querySelectorAll('nav a')
+
+links.forEach(link => {
+    link.style.fontWeight= 'bold'
+})
+
+const hours = new Date().getHours() // get the current hour
+
+let welcomeMessage;
+
+    if (hours >= 4 && hours < 12) {
+        welcomeMessage = "It is too early for this..."
+    }
+
+    else if (hours >= 12 && hours < 17) {
+        welcomeMessage = "It's after noonish right? Time for coffee number 12!"
+    }
+
+    else {
+        welcomeMessage = "Time to daydream about sleep."
+    }
+
+const welcomeDiv = document.getElementById ('welcome');
+welcomeDiv.textContent = welcomeMessage;
+
+// Add a secret message to localStorage
+localStorage.setItem("It's a secret to everybody.", "Uhh...Life finds a way... to store secrets in localStorage.");
